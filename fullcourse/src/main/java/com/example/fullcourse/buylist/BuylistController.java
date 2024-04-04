28c38cffa21e016896076081f4758d9827c00f65
Sanghyeok Moon <ssmunm@naver.com>
2024-04-04 17:38:42 +0900

    buylist

---
+++ fullcourse/src/main/java/com/example/fullcourse/buylist/BuylistController.java
@@ -0,0 +1,5 @@
+package com.example.fullcourse.buylist;
+
+public class BuylistController {
+
+}
---
+++ fullcourse/src/main/java/com/example/fullcourse/buylist/mapper/BuylistMapper.java
@@ -0,0 +1,5 @@
+package com.example.fullcourse.buylist.mapper;
+
+public interface BuylistMapper {
+
+}
