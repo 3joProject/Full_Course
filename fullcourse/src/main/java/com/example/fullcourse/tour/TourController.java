f5b9bcbade57b1b99ae6d866056444a8fa7a09eb
Yerinkimkim <k30191246@gmail.com>
2024-04-05 15:34:20 +0900

    TourController -> /tour

--- fullcourse/src/main/java/com/example/fullcourse/tour/TourController.java
+++ fullcourse/src/main/java/com/example/fullcourse/tour/TourController.java
@@ -1,5 +1,23 @@
 package com.example.fullcourse.tour;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
 public class TourController {
+	
+	@GetMapping("/tour")
+	public String m_insert(Model model) {
+		log.info("/m_insert...");
+
+		model.addAttribute("content", "thymeleaf/member/th_insert");
+		model.addAttribute("title", "회원가입");
+		return "thymeleaf/member/th_layout_main";
+	}
+
 
 }
