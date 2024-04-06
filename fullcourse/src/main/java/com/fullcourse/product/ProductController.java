490e7f479a25ad788433504ef71dfaad2a394d6d
hwangha <ghkdgk488@gmail.com>
2024-04-06 14:27:07 +0900

    [product 패키지]-[Controller, Service, VO 클래스]-[변수명 수정]

--- fullcourse/src/main/java/com/fullcourse/product/ProductController.java
+++ fullcourse/src/main/java/com/fullcourse/product/ProductController.java
@@ -8,64 +8,64 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import java.util.List;
+
 
 @Controller
 @RequestMapping("/product")
 public class ProductController {
 	
-	
+	@Autowired
+	private ProductService productService;
 	
 	@GetMapping("/insert")
 	public String insert(Model model) {
 		model.addAttribute("productVO", new ProductVO());
-		return "product/insert";
+		return "thymeleaf/product/insert";
 	}
 	
 	@PostMapping("/insertOK")
 	public String insertOK(@ModelAttribute ProductVO productVO) {
-//		productService.insert(productVO);
+		productService.insert(productVO);
 		return "redirect:/product/selectAll";
 	}
 	
 	@GetMapping("/update")
-	public String update(@RequestParam("product_num") int product_num, Model model) {
-//		ProductVO productVO = productService.selectOne(product_num);
-//		model.addAttribute("productVO", productVO);
+	public String update(@RequestParam("productNum") int productNum, Model model) {
+		ProductVO productVO = productService.selectOne(productNum);
+		model.addAttribute("productVO", productVO);
 		return "product/update";
 	}
 	
 	@PostMapping("/updateOK")
 	public String updateOK(@ModelAttribute ProductVO productVO) {
-//		productService.update(productVO);
+		productService.update(productVO);
 		return "redirect:/product/selectAll";
 	}
 	
 	@GetMapping("/delete")
-	public String delete(@RequestParam("product_num") int product_num) {
-//		productService.delete(product_num);
+	public String delete(@RequestParam("productNum") int productNum) {
+		productService.delete(productNum);
 		return "redirect:/product/selectAll";
 	}
 	
 	@GetMapping("/selectAll")
 	public String selectAll(Model model) {
-//		List<ProductVO> productList = productService.selectAll();
-//		model.addAttribute("productList", productList);
+		List<ProductVO> productList = productService.selectAll();
+		model.addAttribute("productList", productList);
 		return "product/list";
 	}
 	
 	@GetMapping("/selectOne")
-	public String selectOne(@RequestParam("product_num") int product_num, Model model) {
-//	    ProductVO productVO = productService.selectOne(product_num);
-//	    model.addAttribute("productVO", productVO);
+	public String selectOne(@RequestParam("productNum") int productNum, Model model) {
+	    ProductVO productVO = productService.selectOne(productNum);
+	    model.addAttribute("productVO", productVO);
 	    return "product/view";
 	}
-
 	@GetMapping("/searchList")
 	public String searchList(@RequestParam("searchType") String searchType, @RequestParam("keyword") String keyword, Model model) {
-//	    List<ProductVO> productList = productService.searchList(searchType, keyword);
-//	    model.addAttribute("productList", productList);
+	    List<ProductVO> productList = productService.searchList(searchType, keyword);
+	    model.addAttribute("productList", productList);
 	    return "product/list";
 	}
 	
--- fullcourse/src/main/java/com/fullcourse/product/ProductService.java
+++ fullcourse/src/main/java/com/fullcourse/product/ProductService.java
@@ -1,27 +1,24 @@
 package com.fullcourse.product;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.fullcourse.product.mapper.ProductMapper;
-
 @Service
 public class ProductService {
 	
-	
+	@Autowired
+	private ProductMapper productMapper;
 	
 	public void insert(ProductVO productVO) {
-		
+		productMapper.insert(productVO);
 	}
 	
 	public void update(ProductVO productVO) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public void delete(int product_num) {
+	public void delete(int productNum) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -30,17 +27,19 @@ public class ProductService {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	public ProductVO selectOne(int product_num) {
+	public ProductVO selectOne(int productNum) {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
 	
 	public List<ProductVO> searchList(String searchType, String keyword) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	public int getTotalRows() {
+        return productMapper.getTotalRows();
+    }
 	
 	
 }
