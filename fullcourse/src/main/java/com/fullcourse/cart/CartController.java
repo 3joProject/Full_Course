9b0f5b297a70cb4e60fa64c74aadd1f9c9939c85
hsesis3 <hsesis3@naver.com>
2024-04-06 14:41:11 +0900

    [카트 클래스]-[컨트롤러,서비스,맵퍼]-[selectAll() 변경]

--- fullcourse/src/main/java/com/fullcourse/cart/CartController.java
+++ fullcourse/src/main/java/com/fullcourse/cart/CartController.java
@@ -1,9 +1,13 @@
 package com.fullcourse.cart;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.fullcourse.product.ProductVO;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -12,13 +16,20 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 public class CartController {
 
+	@Autowired
+	private CartService service;
 	
 	@GetMapping("/cart_selectAll")
 	public String cart_selectAll(Model model) {
 		log.info("/cart_selectAll");
 		
+		List<ProductVO> vos = service.selectAll();
 		
-		return new String();
+		log.info("{}",vos);
+		
+		model.addAttribute("vos",vos);
+		
+		return "thymeleaf/cart";
 	}
 	
 	
--- fullcourse/src/main/java/com/fullcourse/cart/CartService.java
+++ fullcourse/src/main/java/com/fullcourse/cart/CartService.java
@@ -6,9 +6,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.fullcourse.cart.mapper.CartMapper;
+import com.fullcourse.product.ProductVO;
 
 @Service
 public class CartService {
+
+	@Autowired
+	private CartMapper mapper;
+	
+	public List<ProductVO> selectAll() {
+
+		return mapper.selectAll();
+	}
 
 
 
