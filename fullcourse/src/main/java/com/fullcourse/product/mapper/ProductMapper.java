1dd925fd657211e1966c033ef3065a47fc6408dc
hwangha <ghkdgk488@gmail.com>
2024-04-06 14:31:33 +0900

    [porduct.mapper 패키지]-[ProductMapper 클래스]-[변수명 수정 ]

--- fullcourse/src/main/java/com/fullcourse/product/mapper/ProductMapper.java
+++ fullcourse/src/main/java/com/fullcourse/product/mapper/ProductMapper.java
@@ -1,17 +1,15 @@
 package com.fullcourse.product.mapper;
 
 import java.util.List;
-
 import org.apache.ibatis.annotations.Mapper;
-
 import com.fullcourse.product.ProductVO;
-
 @Mapper
 public interface ProductMapper {
+	void insert(ProductVO productVO);
 	void update(ProductVO productVO);
-	void delete(int product_num);
+	void delete(int productNum);
 	List<ProductVO> seletctAll();
-	ProductVO selectOne(int product_num);
+	ProductVO selectOne(int productNum);
 	List<ProductVO> searchList(String searchType, String keyword);
-	void insert(ProductVO productVO); 
+	int getTotalRows();
 }
