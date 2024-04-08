5fa6feb758ef7d218fe8ed3bb1ba177139291fbd
Yerinkimkim <k30191246@gmail.com>
2024-04-08 10:17:47 +0900

    [여행지]-[컨트롤러]-[매핑주소입력]

--- fullcourse/src/main/java/com/fullcourse/tour/TourController.java
+++ fullcourse/src/main/java/com/fullcourse/tour/TourController.java
@@ -9,21 +9,93 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class TourController {
-	
+
+	// 여행 페이지 메인
 	@GetMapping("/tour")
-	public String m_insert() {
-		log.info("/m_insert...");
+	public String tourMain() {
+		log.info("/tourMain...");
 
 		return "thymeleaf/blog";
 	}
-	
-	@GetMapping("/tour/details")
-	public String tour_details() {
-		log.info("insert...");
+
+	// 상세정보로 이동
+	@GetMapping("/tour/tourDetails")
+	public String tourDetails() {
+		log.info("tourDetails...");
 
 		return "thymeleaf/single";
 	}
 
-	
+	// 여행지 입력페이지로 이동
+	@GetMapping("/tour/tourInsert")
+	public String tourInsert() {
+		log.info("tourInsert...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지입력 완료 처리 -> tourDeails페이지로 이동?
+	@GetMapping("/tour/tourInsertOK")
+	public String tourInsertOK() {
+		log.info("tourInsertOK...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 정보 수정 페이지 이동
+	@GetMapping("/tour/tourUpdate ")
+	public String tourUpdate() {
+		log.info("tourUpdate ...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 정보 수정 완료처리 -> tourDeails페이지로 이동?
+	@GetMapping("/tour/tourUpdateOK ")
+	public String tourUpdateOK() {
+		log.info("tourUpdateOK ...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 정보 삭제 페이지
+	@GetMapping("/tour/tourDelete")
+	public String tourDelete() {
+		log.info("tourDelete...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 정보 삭제완료 처리 -> selectAll로 돌아가기
+	@GetMapping("/tour/tourDeleteOK ")
+	public String tourDeleteOK() {
+		log.info("tourDeleteOK ...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 정보 검색 ?details랑 겹치나 확인
+	@GetMapping("/tour/tourSelectOne")
+	public String tourSelectOne() {
+		log.info("tourSelectOne ...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 목록
+	@GetMapping("/tour/tourSelectAll")
+	public String tourSelectAll() {
+		log.info("tourSelectAll ...");
+
+		return "thymeleaf/single";
+	}
+
+	// 여행지 목록 검색
+	@GetMapping("/tour/tourSearchList")
+	public String tourSearchList() {
+		log.info("tourSearchList ...");
+
+		return "thymeleaf/single";
+	}
 
 }
