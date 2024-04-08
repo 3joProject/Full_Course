42f5aa623c82d0a5e0c23e140e03e9996d249e00
Yerinkimkim <k30191246@gmail.com>
2024-04-09 00:26:07 +0900

    [여행]-[컨트롤러]-[매핑완료]

--- fullcourse/src/main/java/com/fullcourse/tour/TourService.java
+++ fullcourse/src/main/java/com/fullcourse/tour/TourService.java
@@ -16,29 +16,64 @@ public class TourService {
 	@Autowired
 	private TourMapper mapper;
 
-	public TourVO TourSelectOne(TourVO vo) {
+	public TourVO tourSelectOne(TourVO vo) {
 		
-		return mapper.TourSelectOne(vo);
+		return mapper.tourSelectOne(vo);
 	}
 
-	public int TourInsertOK(TourVO vo) {
-		return mapper.TourInsertOK(vo);
+	public int tourInsertOK(TourVO vo) {
+		return mapper.tourInsertOK(vo);
 	}
 
-	public List<MemberVO> tourSelectAll(int cpage, int pageBlock) {
+	public List<TourVO> tourSelectAll(int cpage, int pageBlock) {
 		int startRow = (cpage - 1) * pageBlock + 1;
 
 		Map<String, Integer> map = new HashMap<String, Integer>();
 		map.put("startRow", startRow-1);
 		map.put("pageBlock", pageBlock);
 		
-		return mapper.TourSelectAll(map);
+		return mapper.tourSelectAll(map);
 	}
 
 
 	public int getTotalRows() {
 		return mapper.getTotalRows();
 	}
+
+	public int tourDeleteOK(TourVO vo) {
+		return mapper.tourDeleteOK(vo);
+	}
+
+	public int tourUpdateOK(TourVO vo) {
+		return mapper.tourUpdateOK(vo);
+	}
+
+	public List<TourVO> searchListPageBlock(String searchKey, String searchWord, int cpage, int pageBlock) {
+		
+	Map<String, Object> map = new HashMap<>();
+	map.put("searchWord", "%"+searchWord+"%");
 	
+	int startRow = (cpage - 1) * pageBlock + 1;
+	map.put("startRow", startRow-1);
+	map.put("pageBlock", pageBlock);
+	
+	if(searchKey.equals("Address")) {
+		return mapper.searchListADDRESS_PAGE(map);
+	}else {
+		return mapper.searchListNAME_PAGE(map);
+	}
+	}
+
+	public int getSearchTotalRows(String searchKey, String searchWord) {
+		Map<String, String> map = new HashMap<>();
+		map.put("searchWord", "%"+searchWord+"%");
+		
+		if(searchKey.equals("address")) {
+			return mapper.search_total_rows_address(map);
+		}else {
+			return mapper.search_total_rows_tourName(map);
+		}
+	}
+
 	
 }
