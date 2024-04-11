4fbb6da835f86f284f8a57dcff153eb8635f7379
hsesis3 <hsesis3@naver.com>
2024-04-11 11:50:12 +0900

    [판매자리뷰]-[패키지 및 클래스 생성]

---
+++ fullcourse/src/main/java/com/fullcourse/seller/sellerReview/SellerReviewService.java
@@ -0,0 +1,5 @@
+package com.fullcourse.seller.sellerReview;
+
+public class SellerReviewService {
+
+}
---
+++ fullcourse/src/main/java/com/fullcourse/seller/sellerReview/mapper/SellerReviewMapper.java
@@ -0,0 +1,5 @@
+package com.fullcourse.seller.sellerReview.mapper;
+
+public interface SellerReviewMapper {
+
+}
