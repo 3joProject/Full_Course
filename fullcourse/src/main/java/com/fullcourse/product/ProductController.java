4dab5e2285ede9ba30af03082ce5cdef7cb8448b
hwangha <ghkdgk488@gmail.com>
2024-04-17 13:33:38 +0900

    [product 패키지]-[controller, service 클래스]-[수정]

--- fullcourse/src/main/java/com/fullcourse/product/ProductController.java
+++ fullcourse/src/main/java/com/fullcourse/product/ProductController.java
@@ -4,14 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.List;
 
 @Slf4j
 @Controller
@@ -21,15 +19,22 @@ public class ProductController {
 	@Autowired
 	private ProductService productService;
 	
-	@GetMapping("/insert")
-	public String insert(Model model) {
+	// 로그인 상태 확인 메소드
+    private boolean isLoggedIn(HttpSession session) {
+        return session.getAttribute("member") != null;
+    }
+    
+    
+    @GetMapping("/insert")
+    public String insert(Model model, HttpSession session) {
+    	if (!isLoggedIn(session)) {
+            return "redirect:/login";  // 로그인 페이지 경로를 확인하고 적절히 수정하세요.
+        }
 		
-		log.info("/insert");
-		
-		model.addAttribute("content", "thymeleaf/product/th_insert");
+		model.addAttribute("content", "thymeleaf/product/insert");
 		model.addAttribute("title", "상품등록");
 		
-		return "thymeleaf/product/th_layout_main";
+		return "thymeleaf/product/layout_main";
 	}
 	
 	@PostMapping("/insertOK")
