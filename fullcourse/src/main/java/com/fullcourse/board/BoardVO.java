17a2a4669f3e810504a0829d5a733be7489334c7
hwangha <ghkdgk488@gmail.com>
2024-04-20 02:32:20 +0900

    chat board 패키지 수정

--- fullcourse/src/main/java/com/fullcourse/board/BoardVO.java
+++ fullcourse/src/main/java/com/fullcourse/board/BoardVO.java
@@ -1,6 +1,6 @@
 package com.fullcourse.board;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,13 +10,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BoardVO {
-	
-	private int boardNum;
-	private String boardTitle;
-	private String boardContent;
-	private String boardWriter;
-	private Date boardDate;
-	private String boardImage;
-	
-	
+    
+    private int boardNum;
+    private String boardTitle;
+    private String boardContent;
+    private String boardWriter;
+    private LocalDateTime boardDate;
+    private String boardImage;
+    
+    
 }
