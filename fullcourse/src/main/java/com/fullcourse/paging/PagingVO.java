dfcb1dd42315024820d26f1bbda0e0112a32f4b1
Yerinkimkim <k30191246@gmail.com>
2024-04-23 10:12:31 +0900

    [여행지목록]-[페이징]-[완료]

---
+++ fullcourse/src/main/java/com/fullcourse/paging/PagingVO.java
@@ -0,0 +1,42 @@
+package com.fullcourse.paging;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+@Data
+public class PagingVO implements Serializable {
+
+    /** 검색조건 */
+    private String searchCondition = "";
+
+    /** 검색Keyword */
+    private String searchKeyword = "";
+
+    /** 현재페이지 */
+    private int pageIndex = 1;
+
+    /** 페이지갯수 */
+    private int pageUnit = 10;
+
+    /** 페이지사이즈 */
+    private int pageSize = 10;
+
+    /** firstIndex */
+    private int firstIndex = 1;
+
+    /** lastIndex */
+    private int lastIndex = 1;
+
+    /** recordCountPerPage */
+    private int recordCountPerPage = 10;
+
+    /** 검색KeywordFrom */
+    private String searchKeywordFrom = "";
+
+    /** 검색KeywordTo */
+    private String searchKeywordTo = "";
+    
+    private String otherSearchCondition = "";
+
+}
