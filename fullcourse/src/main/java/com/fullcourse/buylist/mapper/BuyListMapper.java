5e27cde3efd74d553bc72ee1157df86fcbd2f94b
Sanghyeok Moon <ssmunm@naver.com>
2024-04-23 13:20:51 +0900

    {멤버,구매내역}-{마이페이지수정,구매내역재생성}-{마이페이지수정,구매내역재생성}

--- fullcourse/src/main/java/com/fullcourse/buylist/mapper/BuyListMapper.java
+++ fullcourse/src/main/java/com/fullcourse/buylist/mapper/BuyListMapper.java
@@ -3,13 +3,12 @@ package com.fullcourse.buylist.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
-
+import org.apache.ibatis.annotations.Select;
 
 import com.fullcourse.buylist.BuyListVO;
 
 @Mapper
 public interface BuyListMapper {
-    List<BuyListVO> findByMemberNumPaginated(int memberNum, int offset, int limit);
-    BuyListVO findByBuyNum(int buyNum);
-    int getTotalCount(int memberNum);
+    @Select("SELECT * FROM buylist WHERE buymnum = #{memberNum}")
+    List<BuyListVO> findByMemberNum(int memberNum);
 }
