828b438660247443f7892bd9e2a9a52eb8a47be4
Sanghyeok Moon <ssmunm@naver.com>
2024-04-26 10:44:17 +0900

    {구매내역}-{구매내역수정}-{구매내역수정}

--- fullcourse/src/main/java/com/fullcourse/buylist/mapper/BuyListMapper.java
+++ fullcourse/src/main/java/com/fullcourse/buylist/mapper/BuyListMapper.java
@@ -16,9 +16,10 @@ public interface BuyListMapper {
 	@Select("SELECT * FROM buylist WHERE buymnum = #{memberNum}")
     List<BuyListVO> findByMemberNum(int memberNum);
 	
-	@Select("select * from member m,buylist b,product p\r\n"
+	@Select("select * from member m,buylist b,product p,cart c\r\n"
 			+ "where m.memberNum = b.buyMnum\r\n"
 			+ "and b.buyId = p.productNum\r\n"
+			+ "and c.cartNum = b.buyNum\r\n"
 			+ "and b.buyMnum = #{memberNum}")
 	List<BuyListVO> findByMemberNum2(int memberNum);
 
