2902cba2027d31bf07234fd859ac161b339d87d1
Yerinkimkim <k30191246@gmail.com>
2024-04-29 19:09:31 +0900

    [test]

--- fullcourse/src/main/java/com/fullcourse/route/RouteController.java
+++ fullcourse/src/main/java/com/fullcourse/route/RouteController.java
@@ -1,15 +1,24 @@
 package com.fullcourse.route;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.imageio.ImageIO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.fullcourse.member.MemberVO;
@@ -19,6 +28,7 @@ import com.fullcourse.tour.TourVO;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Controller
 public class RouteController {
@@ -31,37 +41,40 @@ public class RouteController {
 	@Autowired
 	private RouteService service;
 
+	// application.properties 변수를 DI
+	@Value("${file.dir}")
+	String realPath;
+
 	@GetMapping("/route")
 	public String map(Model model, HttpServletRequest request) {
-	    HttpSession session = request.getSession();
-	    Integer memberNum = (Integer) session.getAttribute("memberNum");
-	    
-	    if (memberNum == null) {
-	        return "redirect:/login"; // 로그인 페이지로 리디렉션
-	    }
-	    
-	    MemberVO member = (MemberVO) session.getAttribute("member");
-	    if (member != null) {
-	        boolean loggedIn = true;
-	        log.info("로그인한사람 아이디:" + member.getMemberId());
-	        model.addAttribute("loginId", member.getMemberId());
-	        model.addAttribute("loggedIn", loggedIn);
-	        
-	        log.info("map..");
-	        
-	        String routeUserId = member.getMemberId();
-	        log.info("routeUserId:{}", routeUserId);
+		HttpSession session = request.getSession();
+		Integer memberNum = (Integer) session.getAttribute("memberNum");
 
-	        model.addAttribute("content", "thymeleaf/route/TMAP");
-	        model.addAttribute("routeUserId", routeUserId);
-	        
-	        // index.html 파일을 반환
-	        return "thymeleaf/route/th_routeLayout_main"; 
-	    }
-	    
-	    return "redirect:/login"; // 로그인 페이지로 리디렉션
+		if (memberNum == null) {
+			return "redirect:/login"; // 로그인 페이지로 리디렉션
+		}
+
+		MemberVO member = (MemberVO) session.getAttribute("member");
+		if (member != null) {
+			boolean loggedIn = true;
+			log.info("로그인한사람 아이디:" + member.getMemberId());
+			model.addAttribute("loginId", member.getMemberId());
+			model.addAttribute("loggedIn", loggedIn);
+
+			log.info("map..");
+
+			String routeUserId = member.getMemberId();
+			log.info("routeUserId:{}", routeUserId);
+
+			model.addAttribute("content", "thymeleaf/route/TMAP");
+			model.addAttribute("routeUserId", routeUserId);
+
+			// index.html 파일을 반환
+			return "thymeleaf/route/th_routeLayout_main";
+		}
+
+		return "redirect:/login"; // 로그인 페이지로 리디렉션
 	}
-
 
 	@GetMapping("/route/list")
 	public String tourlist(@RequestParam(defaultValue = "1") int cpage, @RequestParam(defaultValue = "5") int pageBlock,
@@ -101,9 +114,40 @@ public class RouteController {
 
 	// 여행지입력 완료 처리 -> tourDeails페이지로 이동?
 	@PostMapping("/route/tourInsertOK")
-	public String tourInsertOK(TourVO vo) {
+	public String tourInsertOK(@ModelAttribute TourVO vo, @RequestParam("file") MultipartFile file) throws IllegalStateException, IOException {
 		log.info("route..tourInsertOK...");
 		log.info("vo:{}", vo);
+
+//		이미지처리
+		log.info(realPath);
+
+		String originName = vo.getFile().getOriginalFilename();
+
+		log.info("getOriginalFilename:{}", originName);
+
+		if (originName.length() == 0) {
+			vo.setTourImg("default.png"); // 이미지 선택없이 처리할때
+
+		} else {
+			String tourImgName = "img_" + System.currentTimeMillis()
+					+ originName.substring(originName.lastIndexOf("."));
+
+			vo.setTourImg(tourImgName);
+			File uploadFile = new File(realPath, tourImgName);
+//			vo.getFile().transferTo(uploadFile);
+			file.transferTo(uploadFile);
+
+			//// create thumbnail image/////////
+			BufferedImage original_buffer_img = ImageIO.read(uploadFile);
+			BufferedImage thumb_buffer_img = new BufferedImage(50, 50, BufferedImage.TYPE_3BYTE_BGR);
+			Graphics2D graphic = thumb_buffer_img.createGraphics();
+			graphic.drawImage(original_buffer_img, 0, 0, 50, 50, null);
+
+			File thumb_file = new File(realPath, "thumb_" + tourImgName);
+
+			ImageIO.write(thumb_buffer_img, tourImgName.substring(tourImgName.lastIndexOf(".") + 1), thumb_file);
+		}
+//		이미지처리
 
 		int result = toService.tourInsertOK(vo);
 		log.info("result:{}", result);
@@ -154,42 +198,40 @@ public class RouteController {
 		}
 	}
 
-	
 	@GetMapping("/route/update")
 	public String routeUpdate(@RequestParam("routeName") String routeName, Model model, HttpServletRequest request) {
-		
+
 		HttpSession session = request.getSession();
 		MemberVO member = (MemberVO) session.getAttribute("member");
 		log.info("MemberVO:{}", member);
-		
+
 		String routeUserId = member.getMemberId();
 		log.info("routeUserId:{}", routeUserId);
 
+		// routeName을 이용하여 필요한 데이터를 가져오거나 처리하는 작업 수행
 
-	    // routeName을 이용하여 필요한 데이터를 가져오거나 처리하는 작업 수행
-		
 		log.info("routename : {}", routeName);
 		List<RouteVO> routes = service.routeUpdate(routeName);
-		
+
 		log.info("routes : {}", routes);
-	    // 가져온 데이터를 모델에 추가
+		// 가져온 데이터를 모델에 추가
 		model.addAttribute("routes", routes);
-		
+
 		model.addAttribute("routeUserId", routeUserId);
-		
-	    // 수정 페이지의 경로를 반환
-	    return "thymeleaf/route/update";
+
+		// 수정 페이지의 경로를 반환
+		return "thymeleaf/route/update";
 	}
-	
+
 	@PostMapping("route/mypageDelete")
 	public String deleteOK(RouteVO vo, RedirectAttributes redirectAttributes) {
-	    int result = service.mypageDeleteAll(vo);
-	    if (result == 1) {
-	        redirectAttributes.addAttribute("success", true);
-	    } else {
-	        redirectAttributes.addAttribute("success", false);
-	    }
-	    return "redirect:/mypage";
+		int result = service.mypageDeleteAll(vo);
+		if (result == 1) {
+			redirectAttributes.addAttribute("success", true);
+		} else {
+			redirectAttributes.addAttribute("success", false);
+		}
+		return "redirect:/mypage";
 	}
-	
+
 }
